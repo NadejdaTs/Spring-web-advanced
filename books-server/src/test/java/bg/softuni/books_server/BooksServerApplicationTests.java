20745a20bda3218a21b5fd4023c0038619f733a7
NadejdaTs <nadezhda.tsvetanova@gmail.com>
2024-10-28 17:30:37 +0200

    books-server

---
+++ books-server/src/test/java/bg/softuni/books_server/BooksServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.books_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
